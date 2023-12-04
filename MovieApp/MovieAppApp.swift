6c2561eaee5d25a19acdbd891d09364d11fbddef
Sahan Ravindu <sahanrv@outlook.com>
2023-12-05 05:17:55 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Sahan Ravindu on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
